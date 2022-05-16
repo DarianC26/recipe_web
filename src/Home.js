3b8fae596ca09199040dfd42fe79a027bb4fe6fa
DarianC26 <dariancheung@gmail.com>
2022-05-15 22:16:43 -0400

    Can map each meal with title/next images for home page

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,52 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './Home.css';
+import Meals from './Meals';
 
 function Home() {
+    const [meals, setMeals] = useState(null);
+    const [ingredients, setIngredients] = useState('');
+
+    function getMealData() {
+        fetch(
+            `https://api.spoonacular.com/recipes/findByIngredients?apiKey=88592da7a5de4e00b2ccea686f340179&ingredients=apples,+flour,+steak&number=2`
+        )
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+            setMeals(data);
+            })
+            .catch(() => {
+            console.log("error");
+            });
+    }
+    
+    function handleValue(e) {
+        setIngredients(ingredients);
+    }
+
     return (
-        <div className='banner'>
-            <div className='grid'>
-                <div className='title'>
-                    <strong>
-                        Find a Recipe
-                    </strong>
+        <div className='container'>
+            <div className='banner'>
+                <div className='grid'>
+                    <div className='title'>
+                        <strong>
+                            Find a Recipe
+                        </strong>
+                    </div>
+                    <div className='search'>
+                        <input
+                        type="number"
+                        placeholder="Ingredients"
+                        onChange={handleValue}
+                        className='input_box'
+                        />
+                        <button onClick={getMealData}>Get Daily Meal Plan</button>
+                    </div>
                 </div>
-                <div className='search'>
-                    <input
-                    type="number"
-                    placeholder="Calories (e.g. 2000)"
-                    className='input_box'
-                    />
-                </div>
+            </div>
+
+            <div>
+                {meals && <Meals meals={meals} />}
             </div>
         </div>
     );
--- src/Meals.js
+++ src/Meals.js
@@ -1,2 +1,18 @@
-import Reach from 'react';
-import axios from "axios";
+import React from 'react';
+
+function Meals({ meals }) {
+
+    return (
+        <section className="meals">
+        {meals.map((meal) => {
+          return (
+              <p key={meal.id}>
+                  {meal.title}
+              </p>
+          );
+        })}
+      </section>
+    );
+}
+
+export default Meals
